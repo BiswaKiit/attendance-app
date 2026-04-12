ae7168791208f735a7fc265b97c29ae0693a36c9
BiswaKiit <movizworld21@gmail.com>
2026-04-12 11:16:20 +0530

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,25 @@
-self.addEventListener("install", e => {
-    self.skipWaiting();
+const CACHE_NAME = "attendance-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/static/style.css",
+  "/static/favicon.png"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
